5cf07b66fdf7baae44eba4ab1765b6850c7d9125
Armaan <armaan33000@gmail.com>
2024-07-09 22:06:20 +0530

    feat: idk

--- src/app/hospital-dashboard/page.tsx
+++ src/app/hospital-dashboard/page.tsx
@@ -1,6 +1,8 @@
+import DashboardLeftSideBar from "@/components/dashboard-left-side-bar/DashboardLeftSideBar";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
+import "./hospitaldashboardpage.css";
 
 type Props = {};
 
@@ -8,7 +10,49 @@ const HospitalDashboardPage = (props: Props) => {
   return (
     <>
       <Navbar />
-      <div className="hospitalDashboardPage"></div>
+      <div className="hospitalDashboardPage flex flex-row justify-start items-start">
+        <DashboardLeftSideBar />
+
+        <div className="ml-16">
+          <center>
+            <input
+              type="button"
+              value="Add room"
+              className="w-28 text-white rounded-sm px-4 py-2 cursor-pointer transition-all hover:bg-[#c62222] bg-[#991b1b]"
+            />
+            <div className="flex flex-row justify-center items-center">
+              <h4
+                className="text-white font-thin mt-4 text-2xl"
+                style={{ fontFamily: "Ga Maamli, sans-serif" }}
+              >
+                Code: 374311
+              </h4>
+              <img
+                src="https://cdn-icons-gif.flaticon.com/11677/11677427.gif"
+                className="w-6 mt-3 ml-2 cursor-pointer "
+              />
+            </div>
+            <br />
+
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center items-center"></div>
+            </div>
+          </center>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Footer />
     </>
   );
--- src/components/dashboard-left-side-bar/DashboardLeftSideBar.tsx
+++ src/components/dashboard-left-side-bar/DashboardLeftSideBar.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import './dashboardleftsidebar.css';
+import React from "react";
+import "./dashboardleftsidebar.css";
 
-type Props = {}
+type Props = {};
 
 const DashboardLeftSideBar = (props: Props) => {
   return (
-        <>
-            <div className="dashboardLeftSideBar">
+    <>
+      <div className="dashboardLeftSideBar w-72  flex flex-col  bg-slate-50 justify-center items-start rounded-sm">
+        <div className="item flex flex-row justify-center p-2 items-center bg-slate-50 w-full hover:bg-slate-300 cursor-pointer">
+          <img
+            src="https://cdn-icons-gif.flaticon.com/13099/13099870.gif"
+            className="w-12 mix-blend-multiply"
+          />
+          <h4
+            className="text-lg ml-4"
+            style={{ fontFamily: "Poppins, sans-serif" }}
+          >
+            Hospital Management
+          </h4>
+        </div>
+        <div className="w-full bg-[#1e1e1e] h-[1px]"></div>
+        <div className="item flex flex-row justify-start p-2 items-center bg-slate-50 w-full hover:bg-slate-300 cursor-pointer">
+          <img
+            src="https://cdn-icons-gif.flaticon.com/14642/14642503.gif"
+            className="w-12 mix-blend-multiply"
+          />
+          <h4
+            className="text-lg ml-4"
+            style={{ fontFamily: "Poppins, sans-serif" }}
+          >
+            Notifications
+          </h4>
+        </div>
+      </div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-
-}
-
-export default DashboardLeftSideBar
+export default DashboardLeftSideBar;
